e38b5b67eb67a5a00d9e6a8ab858eda50b7a3579
s0wuk1 <2831643156@qq.com>
2026-06-16 20:09:50 +0800

    fix log path

--- common/log_util.py
+++ common/log_util.py
@@ -1,26 +1,23 @@
 import logging
 import os
-# 创建日志器
+
 logger = logging.getLogger()
-# 设置日志级别
 logger.setLevel(logging.INFO)
-# 日志输出格式
+
 formatter = logging.Formatter(
     "%(asctime)s - %(levelname)s - %(message)s"
 )
-# 创建文件处理器
-# os.makedirs("../logs", exist_ok=True)
+
+log_dir = "logs"
+
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
 file_handler = logging.FileHandler(
-    "../logs/test.log",
+    os.path.join(log_dir, "test.log"),
     encoding="utf-8"
 )
-# 设置格式
-file_handler.setFormatter(formatter)
-# 创建控制台处理器
-console_handler = logging.StreamHandler()
-# 设置格式
-console_handler.setFormatter(formatter)
 
-# 添加处理器
+file_handler.setFormatter(formatter)
+
 logger.addHandler(file_handler)
-logger.addHandler(console_handler)
